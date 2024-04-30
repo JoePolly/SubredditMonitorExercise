5b60b3a8abb62dddeaac4d895ef3a5be9080fa4d
Joseph Polly <Joseph@Polly.Family>
2024-04-30 17:56:44 -0400

    feat: create basic config class that can be loaded via System.Text.Json

---
+++ SubredditMonitorExercise.Types/Config.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+using JetBrains.Annotations;
+
+namespace SubredditMonitorExercise.Types;
+
+[PublicAPI]
+public class Config
+{
+    [JsonPropertyName("subreddit")]
+    [JsonRequired]
+    public required string Subreddit { get; set; }
+
+    [JsonPropertyName("clientId")]
+    [JsonRequired]
+    public required string ClientId { get; set; }
+    
+    [JsonPropertyName("clientSecret")]
+    [JsonRequired]
+    public required string ClientSecret { get; set; }
+}
