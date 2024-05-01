61cd91216f297076caf19b29f158e2b0340b4eb7
Joseph Polly <Joseph@Polly.Family>
2024-05-01 10:10:20 -0400

    feat: Add StatisticsWriter.cs which will routinely write statistics to a JSON file in the executing directory.

---
+++ SubredditMonitorExercise/BackgroundServices/StatisticsWriter.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using SubredditMonitorExercise.Types.Interfaces;
+
+namespace SubredditMonitorExercise.BackgroundServices;
+
+public sealed class StatisticsWriter : BackgroundService
+{
+    private readonly IPostStore _postStore;
+    private readonly string _filename;
+    private readonly TimeSpan _writeInterval;
+
+    public StatisticsWriter(IConfiguration configuration, IPostStore postStore)
+    {
+        _postStore = postStore;
+        _filename = configuration.GetValue("StatisticsWriter:Filename", "statistics.json")!;
+        _writeInterval = TimeSpan.FromMilliseconds(configuration.GetValue("StatisticsWriter:IntervalMs", 5000));
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            // Get file write handle
+            var file = new FileInfo(_filename);
+            await using var stream = file.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+            await using var writer = new StreamWriter(stream);
+
+            var postCountsByUser = _postStore.GetPostCountsByUser();
+            
+            if (postCountsByUser.Count == 0)
+            {
+                // No posts have been saved yet.
+                
+                await Task.Delay(_writeInterval, stoppingToken);
+                continue;
+            }
+
+            var userWithMostPosts = postCountsByUser.MaxBy(pair => pair.Value).Key;
+            var totalPosts = postCountsByUser.Sum(pair => pair.Value);
+            var averagePostsPerUser = totalPosts / postCountsByUser.Count;
+
+            var mostUpvotedPost = _postStore.GetTopPosts(1).SingleOrDefault();
+
+            await JsonSerializer.SerializeAsync(writer.BaseStream, new
+                {
+                    UserWithMostPosts = new {
+                        UserName = userWithMostPosts,
+                        PostCount = postCountsByUser[userWithMostPosts] 
+                    },
+                    AveragePostsPerUser = averagePostsPerUser,
+                    TotalPosts = totalPosts,
+                    MostUpvotedPost = mostUpvotedPost
+                },
+                new JsonSerializerOptions { WriteIndented = true }, stoppingToken);
+
+            await Task.Delay(_writeInterval, stoppingToken);
+        }
+    }
+}
