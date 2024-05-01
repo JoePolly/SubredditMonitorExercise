38e76f44c8b11605a767652cd5ee4975bafdbd08
Joseph Polly <Joseph@Polly.Family>
2024-05-01 09:49:26 -0400

    feat: Use Microsoft.Extensions.Hosting to provide dependency injection for the conifg. Set up the config file for use with IConfiguration.

---
+++ SubredditMonitorExercise/Storage/PostFeed.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SubredditMonitorExercise.Types.Interfaces;
+
+namespace SubredditMonitorExercise.Storage;
+
+/// <summary>
+/// A feed of posts that can be enqueued and dequeued.
+/// </summary>
+public class PostFeed : IPostFeed
+{
+    private readonly ILogger _logger;
+    private readonly int _warningThreshold;
+    private readonly Queue<string> _postQueue = new();
+    private readonly Dictionary<string, ISocialMediaPost> _posts = new();
+
+    private readonly object _lock = new();
+
+    public PostFeed(ILogger<PostFeed> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _warningThreshold = configuration.GetValue("PostFeed:WarningThreshold", 10);
+    }
+
+    public void EnqueuePost(ISocialMediaPost post)
+    {
+        var count = 0;
+        lock (_lock)
+        {
+            if (_posts.ContainsKey(post.Id))
+            {
+                // Check if the incoming post is newer than the one we have
+                if (_posts[post.Id].FetchTime < post.FetchTime)
+                {
+                    _posts[post.Id] = post;
+                    _logger.LogInformation("Updated post {PostId} ({PostUrl}) with score {PostScore}", post.Id,
+                        post.Url, post.Score);
+                }
+
+                return;
+            }
+
+            _posts.Add(post.Id, post);
+            _postQueue.Enqueue(post.Id);
+            count = _postQueue.Count;
+        }
+
+        _logger.LogTrace(
+            "Enqueued post {PostId} from {PostAuthor} with title {PostTitle} ({PostUrl}) with score {PostScore} at {PostCreated}",
+            post.Id, post.Author, post.Title, post.Url, post.Score, post.Created);
+
+        if (count >= _warningThreshold) _logger.LogWarning("Post queue has reached {PostCount} posts", count);
+    }
+
+    public ISocialMediaPost? DequeuePost()
+    {
+        lock (_lock)
+        {
+            if (_postQueue.Count <= 0) return null;
+
+            var postId = _postQueue.Dequeue();
+            var post = _posts[postId];
+            _posts.Remove(postId);
+            return post;
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        lock (_lock)
+        {
+            return _postQueue.Count <= 0;
+        }
+    }
+}
---
+++ SubredditMonitorExercise/Storage/PostMemoryStore.cs
@@ -0,0 +1,58 @@
+﻿using SubredditMonitorExercise.Types.Interfaces;
+
+namespace SubredditMonitorExercise.Storage;
+
+public class PostMemoryStore : IPostStore
+{
+    private readonly Dictionary<string, ISocialMediaPost> _posts = new();
+
+    public void Insert(ISocialMediaPost post)
+    {
+        _posts.Add(post.Id, post);
+    }
+
+    public bool Delete(string id)
+    {
+        return _posts.Remove(id);
+    }
+
+    public bool Exists(string id)
+    {
+        return _posts.ContainsKey(id);
+    }
+
+    public void Clear()
+    {
+        _posts.Clear();
+    }
+
+    public IReadOnlyCollection<ISocialMediaPost> GetAllPosts()
+    {
+        return _posts.Values.ToList();
+    }
+
+    public IReadOnlyCollection<ISocialMediaPost> GetAllPostsByUser(string user)
+    {
+        return _posts.Values.Where(p => p.Author == user).ToList();
+    }
+
+    public IReadOnlyCollection<ISocialMediaPost> GetTopPosts(int count = 10)
+    {
+        return _posts.Values.OrderByDescending(p => p.Score).Take(count).ToList();
+    }
+
+    public IReadOnlyCollection<ISocialMediaPost> GetTopPostsByUser(string user, int count = 10)
+    {
+        return _posts.Values.Where(p => p.Author == user).OrderByDescending(p => p.Score).Take(count).ToList();
+    }
+
+    public IReadOnlyDictionary<string, int> GetPostCountsByUser()
+    {
+        return _posts.Values.GroupBy(p => p.Author).ToDictionary(g => g.Key, g => g.Count());
+    }
+
+    public void SetPostScore(string postId, int postScore)
+    {
+        if (_posts.TryGetValue(postId, out var post)) post.Score = postScore;
+    }
+}
